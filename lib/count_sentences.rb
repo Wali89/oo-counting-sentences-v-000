bf885246463d9cb6e3ead4ccc4ab5f76736715d9
Wali Rahman <wali.rahman89@yahoo.com>
2017-12-25 23:39:39 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,8 +15,13 @@ class String
   end
 
   def count_sentences
+    binding.pry
     self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
     binding.pry
   end
 
 end
+
+"This, well, is a sentence. This is too!! And so is this, I think? Woo..." = String.new
+
+"This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
